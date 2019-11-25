46a809794d820fdad78aa7a6a3cbd53fb8d7643f
jbony88 <info@platowdesigns.com>
2019-11-25 08:33:47 -0600

    completed reverse int function

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  // If n is less than zero return negative one and then multiply by reversed string
+  // if n is more than zero return positive 1 and then multiply by reveresed string
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
